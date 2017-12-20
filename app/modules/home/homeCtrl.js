eb6aa4524c567dcbb0c06d670af41c3f17d7cdf7
Michela Zucca <michela.zucca@heig-vd.ch>
2017-12-20 19:33:42 +0100

    Mise en place des graphiques

--- app/modules/home/homeCtrl.js
+++ app/modules/home/homeCtrl.js
@@ -38,6 +38,103 @@
 			console.log("click");
 		}	
 
+		vm.lineChartOppened = {
+			
+		}
+
+
+		vm.lineChartClosed = {
+			
+		}
+
+		/* Encapsulation ne fonctionne pas
+		vm.lineChart = {
+			labels : ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+			series : ['?????'],
+			data : [65, 59, 80, 81, 56, 55, 40],
+			datasetOverride : { yAxisID: 'y-axis-1' },
+			options : {
+				responsive: true,
+				maintainAspectRatio: true,
+			  scales: {
+				yAxes: [
+				  {
+					id: 'y-axis-1',
+					type: 'linear',
+					display: true,
+					position: 'left'
+				  },
+				]
+			  }
+			},
+			colors : [
+				{
+				  backgroundColor: "rgba(159,204,0, 0.2)",
+				  pointBackgroundColor: "rgba(159,204,0, 1)",
+				  pointHoverBackgroundColor: "rgba(159,204,0, 0.8)",
+				  borderColor: "rgba(159,204,0, 1)",
+				  pointBorderColor: '#fff',
+				  pointHoverBorderColor: "rgba(159,204,0, 1)"
+				},"rgba(250,109,33,0.5)","#9a9a9a","rgb(233,177,69)"
+			  ],
+		}*/
+
+
+		/* LINE CHART VERSION BRUT TEMPORAIRE */
+		vm.labelsLineChart = ["January", "February", "March", "April", "May", "June", "July"];
+		vm.seriesLineChart = ['Series A'];
+		vm.dataLineChart = [
+		  [65, 59, 80, 81, 56, 55, 40],
+		];
+		vm.onClick = function (points, evt) {
+		  console.log(points, evt);
+		};
+		vm.datasetOverrideLineChart = { yAxisID: 'y-axis-1' };
+		vm.optionsLineChart = {
+			responsive: true,
+			maintainAspectRatio: true,
+		  scales: {
+			yAxes: [
+			  {
+				id: 'y-axis-1',
+				type: 'linear',
+				display: true,
+				position: 'left'
+			  },
+			]
+		  }
+		};
+		vm.colorsLineChart = [
+            {
+              backgroundColor: "rgba(159,204,0, 0.2)",
+              pointBackgroundColor: "rgba(159,204,0, 1)",
+              pointHoverBackgroundColor: "rgba(159,204,0, 0.8)",
+              borderColor: "rgba(159,204,0, 1)",
+              pointBorderColor: '#fff',
+              pointHoverBorderColor: "rgba(159,204,0, 1)"
+            },"rgba(250,109,33,0.5)","#9a9a9a","rgb(233,177,69)"
+		  ];
+		
+		  /* BAR CHART VERSION BRUT TEMPORAIRE*/
+		  vm.labelsBarChart = ['2ème', '1er', '3ème'];
+		  vm.seriesBarChart = ['Jean', 'Pierre', 'Jack'];
+		
+		  vm.dataBarChart = [
+			[13],
+			[18],
+			[9],
+		  ];
+
+		  vm.optionsBarChart = {
+			// All of my other bar chart option here
+			scales: {
+				yAxes: [{
+					ticks: {
+						beginAtZero:true
+					}
+				}]
+			}
+		};
 	}
 
 })();
