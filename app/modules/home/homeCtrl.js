97ecca8b205b855ffb40de4847bde0a76e07304c
Michela Zucca <michela.zucca@heig-vd.ch>
2017-12-29 12:58:22 +0100

    Modification du conroller, factorisation des charts, modifications du formulaire

--- app/modules/home/homeCtrl.js
+++ app/modules/home/homeCtrl.js
@@ -28,53 +28,51 @@
 		vm.version = "1.0.0";
 		vm.listFeatures = homeService.getFeaturesList();
 
-		vm.repoGitHub = {
-			repo : 'https://github.com/<owner>/<repo>',
+		/* Formulaire */
+		vm.form = {
+			repoGitHub : {
+				selectedRepo : 'https://github.com/<owner>/<repo>',
+			},
+			period : {
+				availableOptions :[
+					{id: '0', name: '> 1 an'},
+					{id: '1', name: '1 an'},
+					{id: '2', name: '6 mois'},
+					{id: '3', name: '3 mois'},
+					{id: '4', name: '1 mois'},
+					{id: '5', name: '1 semaine'},
+					{id: '6', name: '1 jour'},
+				],
+				selectedOption : {id: '0', name: '> 1 an'}, // default option
+			},
+			groupment : {
+				availableOptions :[
+					{id: '1', name: '1 an'},
+					{id: '2', name: '6 mois'},
+					{id: '3', name: '3 mois'},
+					{id: '4', name: '1 mois'},
+					{id: '5', name: '2 semaines'},
+					{id: '6', name: '1 semaine'},
+				],
+				selectedOption : {id: '1', name: '1 an'}, // default option
+			},
+			/* Fonction appelée au clique du bouton search 
+				 Doit appeler le service http pour récuépérer les données selon la sélection */
+			apply :function(){
+				console.log("click"+ vm.form.repoGitHub.selectedRepo + " "+ vm.form.period.selectedOption.name + " "+ vm.form.groupment.selectedOption.name);
+			},
 		}
 
-		vm.period = {
-			availableOptions :[
-				{id: '0', name: '> 1 an'},
-				{id: '1', name: '1 an'},
-				{id: '2', name: '6 mois'},
-				{id: '3', name: '3 mois'},
-				{id: '4', name: '1 mois'},
-				{id: '5', name: '1 semaine'},
-				{id: '6', name: '1 jour'},
-			],
-			selectedOption : {id: '0', name: '> 1 an'}, // default option
-		}
 
-		vm.groupment = {
-			availableOptions :[
-				{id: '1', name: '1 an'},
-				{id: '2', name: '6 mois'},
-				{id: '3', name: '3 mois'},
-				{id: '4', name: '1 mois'},
-				{id: '5', name: '2 semaines'},
-				{id: '6', name: '1 semaine'},
-			],
-			selectedOption : {id: '1', name: '1 an'}, // default option
-		}
+		/* Data des lineChart */
+		vm.dataLineChartOI = [ [65, 59, 80, 81, 56, 55, 40], ];
+		vm.dataLineChartCI = [ [40, 32, 93, 81, 20, 55, 40], ];
+		vm.dataLineChartI = [ [21, 43, 67, 81, 56, 55, 40], ];
 
-		vm.apply = function(repo,periode,groupement){
-			console.log("click"+ repo + " "+ periode + " "+ groupement);
-		}	
-
-		vm.lineChartOppened = {
-			
-		}
-
-
-		vm.lineChartClosed = {
-			
-		}
-
-		/* Encapsulation ne fonctionne pas
+		/* Configurations des linesChart */
 		vm.lineChart = {
-			labels : ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-			series : ['?????'],
-			data : [65, 59, 80, 81, 56, 55, 40],
+			labels :  ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+			series : ['Series A'],
 			datasetOverride : { yAxisID: 'y-axis-1' },
 			options : {
 				responsive: true,
@@ -99,65 +97,34 @@
 				  pointBorderColor: '#fff',
 				  pointHoverBorderColor: "rgba(159,204,0, 1)"
 				},"rgba(250,109,33,0.5)","#9a9a9a","rgb(233,177,69)"
-			  ],
-		}*/
+			],
+			onClick : function (points, evt) {
+				console.log(points, evt);
+			},
+		}
 
+		/* Data des BarChart */
+		vm.seriesBarChartOI = ['Jean', 'Pierre', 'Jack'];
+		vm.dataBarChartOI = [	[3], [5], [1], ];
+		vm.seriesBarChartCI = ['Anne', 'Jack', 'Fred'];
+		vm.dataBarChartCI = [	[2], [18], [9], ];
+		vm.seriesBarChartI = ['Joe', 'Pascal', 'Charlie'];
+		vm.dataBarChartI = [	[13], [14], [9], ];
 
-		/* LINE CHART VERSION BRUT TEMPORAIRE */
-		vm.labelsLineChart = ["January", "February", "March", "April", "May", "June", "July"];
-		vm.seriesLineChart = ['Series A'];
-		vm.dataLineChart = [
-		  [65, 59, 80, 81, 56, 55, 40],
-		];
-		vm.onClick = function (points, evt) {
-		  console.log(points, evt);
-		};
-		vm.datasetOverrideLineChart = { yAxisID: 'y-axis-1' };
-		vm.optionsLineChart = {
-			responsive: true,
-			maintainAspectRatio: true,
-		  scales: {
-			yAxes: [
-			  {
-				id: 'y-axis-1',
-				type: 'linear',
-				display: true,
-				position: 'left'
-			  },
-			]
-		  }
-		};
-		vm.colorsLineChart = [
-            {
-              backgroundColor: "rgba(159,204,0, 0.2)",
-              pointBackgroundColor: "rgba(159,204,0, 1)",
-              pointHoverBackgroundColor: "rgba(159,204,0, 0.8)",
-              borderColor: "rgba(159,204,0, 1)",
-              pointBorderColor: '#fff',
-              pointHoverBorderColor: "rgba(159,204,0, 1)"
-            },"rgba(250,109,33,0.5)","#9a9a9a","rgb(233,177,69)"
-		  ];
-		
-		  /* BAR CHART VERSION BRUT TEMPORAIRE*/
-		  vm.labelsBarChart = ['2ème', '1er', '3ème'];
-		  vm.seriesBarChart = ['Jean', 'Pierre', 'Jack'];
-		
-		  vm.dataBarChart = [
-			[13],
-			[18],
-			[9],
-		  ];
+		/* Configuration des BarChart */
+		vm.BarChart = {
+			labels : ['2ème', '1er', '3ème'],
+			options : {
+				scales: {
+					yAxes: [{
+						ticks: {
+							beginAtZero:true
+						}
+					}]
+				}
+			},
+		}
 
-		  vm.optionsBarChart = {
-			// All of my other bar chart option here
-			scales: {
-				yAxes: [{
-					ticks: {
-						beginAtZero:true
-					}
-				}]
-			}
-		};
 	}
 
 })();
