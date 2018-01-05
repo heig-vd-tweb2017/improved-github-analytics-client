007875020a5d3197362d5bea579a45bed6d96dac
Michela Zucca <michela.zucca@heig-vd.ch>
2018-01-05 13:50:18 +0100

    avant pull

--- app/modules/home/homeService.js
+++ app/modules/home/homeService.js
@@ -18,9 +18,9 @@
 
 		/* Data récupérés sur le serveur */
 		var data = {
-			lineChartOI : [ [1,2,3,4,5,6,7,8,9,10] ],
-			lineChartCI : [ [65, 59, 80, 81, 56, 55, 40], ],
-			lineChartI : [ [10 ,2,32,4,5,32 ,7,8,86,10] ],
+			lineChartOI : [ [1, 2, 3, 4, 5, 6, 7, 8, 9, 10] ],
+			lineChartCI : [ [65, 59, 80, 81, 56, 12, 40, 76, 98, 10, 28, 9, 76], ],
+			lineChartI : [ [10 ,2, 32, 4, 5, 32, 7, 8, 86, 10] ],
 			BarChartOI : {
 				series : ['Jean', 'Pierre', 'Jack'],
 				data : [ [32], [56], [4], ],
@@ -39,8 +39,25 @@
 			getData : getData
 		};
 
+		var url = 'https://evening-garden-52901.herokuapp.com';
+		var repo = 'angular';
+		var owner = 'angular';
+
 		function getData(){
 			/* Ici doit se faire la requête au serveur */
+			$http({
+				method: 'GET',
+				url :`${url}/api/opened-issues/${owner}/${repo}`
+			}).then(function successCallback(response) {
+				var resp = response.data;
+				console.log(resp);
+				// this callback will be called asynchronously
+				// when the response is available
+			}, function errorCallback(response) {
+				console.log('error');
+				// called asynchronously if an error occurs
+				// or server returns response with an error status.
+			});
 			return data;
 		}
 
